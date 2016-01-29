88ff69a27d1a2809ee03001b3a26f81682ba5aed
Jose Rufino <jose.rufino@smartbox.com>
2016-01-29 16:12:43 +0000

    EAI Fixed author block

--- Services/Soap/TypeConverters/DateTimeTypeConverter.php
+++ Services/Soap/TypeConverters/DateTimeTypeConverter.php
@@ -3,11 +3,7 @@
 namespace Smartbox\ApiBundle\Services\Soap\TypeConverters;
 
 use BeSimple\SoapServer\Exception\SenderSoapFault;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
-/**
- * @author Christian Kerl <christian-kerl@web.de>
- */
 class DateTimeTypeConverter extends \BeSimple\SoapCommon\Converter\DateTimeTypeConverter
 {
     public function convertXmlToPhp($data)

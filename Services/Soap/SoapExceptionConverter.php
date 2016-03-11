9fffe1146e0fcfd476289b25bb7a24385facbe0c
Luciano.Mammino <Luciano.Mammino@smartbox.com>
2016-03-11 17:11:40 +0000

    TransactionId added responses of requests that failed

--- Services/Soap/SoapExceptionConverter.php
+++ Services/Soap/SoapExceptionConverter.php
@@ -6,7 +6,9 @@ use BeSimple\SoapServer\Exception\ReceiverSoapFault;
 use BeSimple\SoapServer\Exception\SenderSoapFault;
 use Psr\Log\LoggerInterface;
 use Smartbox\Integration\FrameworkBundle\Exceptions\InvalidMessageException;
+use Smartbox\Integration\PlatformBundle\TransactionId\TransactionId;
 use Symfony\Component\Debug\Exception\FatalErrorException;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -23,14 +25,45 @@ class SoapExceptionConverter
     /** @var \Psr\Log\LoggerInterface */
     protected $logger;
 
+    /** @var RequestStack */
+    protected $requestStack;
+
     /**
      * SoapExceptionConverter constructor.
      *
-     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Psr\Log\LoggerInterface  $logger
+     * @param RequestStack              $requestStack
      */
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, RequestStack $requestStack)
     {
         $this->logger = $logger;
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @param string $class
+     * @param string $message
+     * @param int    $code
+     * @param string $actor
+     *
+     * @return \SoapFault
+     */
+    protected function createSoapFault($class, $message, $code = 0, $actor = null)
+    {
+        $transactionId = $this->requestStack->getMasterRequest()->server->get(TransactionId::SERVER_PARAM_NAME, null);
+        $detail = [TransactionId::SOAP_HEADER_NAME => $transactionId];
+        switch ($class) {
+            case SenderSoapFault::class:
+            case ReceiverSoapFault::class:
+                return new $class($message, $actor, $detail);
+                break;
+            case \SoapFault::class:
+                return new $class($code, $message, $actor, $detail);
+                break;
+        }
+
+        // if another class is given defaults to a simple generic SoapFault
+        return new \SoapFault($code, 'SOAP-ERROR: '. $message, $actor, $detail);
     }
 
     public function onKernelException(GetResponseForExceptionEvent $event)
@@ -57,27 +90,27 @@ class SoapExceptionConverter
             }
 
             if ($exception instanceof UnauthorizedHttpException) {
-                $event->setException( new SenderSoapFault('Not authorized'));
+                $event->setException($this->createSoapFault(SenderSoapFault::class, 'Not authorized'));
                 return;
             }
 
             if ($exception instanceof AuthenticationException) {
-                $event->setException( new SenderSoapFault('Authentication failed'));
+                $event->setException($this->createSoapFault(SenderSoapFault::class, 'Authentication failed'));
                 return;
             }
 
             if ($exception instanceof BadRequestHttpException) {
-                $event->setException(new SenderSoapFault($exception->getMessage()));
+                $event->setException($this->createSoapFault(SenderSoapFault::class, $exception->getMessage()));
                 return;
             }
 
             if ($exception instanceof InvalidMessageException) {
-                $event->setException(new SenderSoapFault($exception->getMessage()));
+                $event->setException($this->createSoapFault(SenderSoapFault::class, $exception->getMessage()));
                 return;
             }
 
             if ($exception instanceof AccessDeniedHttpException) {
-                $event->setException(new SenderSoapFault($exception->getMessage()));
+                $event->setException($this->createSoapFault(SenderSoapFault::class, $exception->getMessage()));
                 return;
             }
 
@@ -89,7 +122,7 @@ class SoapExceptionConverter
                     ),
                     ['exception' => $exception]
                 );
-                $event->setException(new ReceiverSoapFault("Internal error"));
+                $event->setException($this->createSoapFault(ReceiverSoapFault::class, "Internal error"));
                 return;
             }
         }

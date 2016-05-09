576fb76dfc30749d96fd590ab8f1ec357351b3d5
David Camprubi <david.camprubi@smartbox.com>
2016-05-09 08:35:57 +0000

    Create ServerBuilderTest and add the case to setNotExistingServerClass

---
+++ Tests/Services/Soap/ServerBuilderTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Smartbox\ApiBundle\Tests\Services\Soap;
+
+use Smartbox\ApiBundle\Services\Soap\ServerBuilder;
+
+class ServerBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    private $serverBuilder;
+
+    public function setUp()
+    {
+        $this->serverBuilder = new ServerBuilder;
+    }
+
+    public function testSetNotExistingServerClass()
+    {
+        $this->setExpectedException(\InvalidArgumentException::class);
+
+        $this->serverBuilder->setServerClass('\Set\Non\Existing\Class');
+    }
+}

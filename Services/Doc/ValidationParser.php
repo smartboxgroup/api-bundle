3de27e0cfa58f5d9ad47990cc23e3e8a4abe726b
Jose Rufino <jose.rufino@smartbox.com>
2016-06-07 13:28:55 +0100

    Proposed review changes

--- Services/Doc/ValidationParser.php
+++ Services/Doc/ValidationParser.php
@@ -14,6 +14,7 @@ use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints\Count;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
 use Symfony\Component\Validator\Mapping\PropertyMetadata;
+use Symfony\Component\Validator\Constraints\Type;
 
 class ValidationParser extends \Nelmio\ApiDocBundle\Parser\ValidationParser implements ParserInterface, PostParserInterface
 {
@@ -93,6 +94,8 @@ class ValidationParser extends \Nelmio\ApiDocBundle\Parser\ValidationParser impl
             $validationParams = array();
 
             $validationParams['default'] = isset($defaults[$property]) ? $defaults[$property] : null;
+            $validationParams['groups'] = $groups;
+            $validationParams['version'] = $version;
 
             $pds = $meta->getPropertyMetadata($property);
             /** @var PropertyMetadata $propertyMetadata */
@@ -114,16 +117,22 @@ class ValidationParser extends \Nelmio\ApiDocBundle\Parser\ValidationParser impl
                 }
             }
 
-            if (in_array($validationParams['class'], $visited)) {
-                continue;
+            $typeKey = null;
+
+            if(array_key_exists('class',$validationParams) && $validationParams['class']){
+                $typeKey = $validationParams['class'].$version;
+
+                if(!empty($groups)){
+                    $typeKey.= join('',$groups);
+                }
             }
 
             // check for nested classes with All constraint
-            if (
+            if ($typeKey && !in_array($typeKey, $visited) &&
                 isset($validationParams['class']) &&
                 null !== $this->factory->getMetadataFor($validationParams['class'])
             ) {
-                $visited[] = $validationParams['class'];
+                $visited[] = $typeKey;
                 $validationParams['children'] = $this->doParse($validationParams['class'], $visited, $version, $groups);
             }
 
@@ -160,15 +169,127 @@ class ValidationParser extends \Nelmio\ApiDocBundle\Parser\ValidationParser impl
     }
 
     /**
-     * {@inheritdoc}
+     * Create a valid documentation parameter based on an individual validation Constraint.
+     * Currently supports:
+     *  - NotBlank/NotNull
+     *  - Type
+     *  - Email
+     *  - Url
+     *  - Ip
+     *  - Length (min and max)
+     *  - Choice (single and multiple, min and max)
+     *  - Regex (match and non-match)
+     *
+     * @param  Constraint $constraint The constraint metadata object.
+     * @param  array      $vparams    The existing validation parameters.
+     * @return mixed      The parsed list of validation parameters.
      */
     protected function parseConstraint(Constraint $constraint, $vparams, $className, &$visited = array())
     {
-        $validationParams = parent::parseConstraint($constraint, $vparams, $className, $visited);
-        $validationParams = $this->parseCountConstraint($constraint, $validationParams);
-        $validationParams = $this->parseDataType($validationParams);
+        $class = substr(get_class($constraint), strlen('Symfony\\Component\\Validator\\Constraints\\'));
 
-        return $validationParams;
+        $vparams['actualType'] = DataTypes::STRING;
+        $vparams['subType'] = null;
+
+        switch ($class) {
+            case 'NotBlank':
+                $vparams['format'][] = '{not blank}';
+            case 'NotNull':
+                $vparams['required'] = true;
+                break;
+            case 'Type':
+                if (isset($this->typeMap[$constraint->type])) {
+                    $vparams['actualType'] = $this->typeMap[$constraint->type];
+                }
+                $vparams['dataType'] = $constraint->type;
+                break;
+            case 'Email':
+                $vparams['format'][] = '{email address}';
+                break;
+            case 'Url':
+                $vparams['format'][] = '{url}';
+                break;
+            case 'Ip':
+                $vparams['format'][] = '{ip address}';
+                break;
+            case 'Date':
+                $vparams['format'][] = '{Date YYYY-MM-DD}';
+                $vparams['actualType'] = DataTypes::DATE;
+                break;
+            case 'DateTime':
+                $vparams['format'][] = '{DateTime YYYY-MM-DD HH:MM:SS}';
+                $vparams['actualType'] = DataTypes::DATETIME;
+                break;
+            case 'Time':
+                $vparams['format'][] = '{Time HH:MM:SS}';
+                $vparams['actualType'] = DataTypes::TIME;
+                break;
+            case 'Length':
+                $messages = array();
+                if (isset($constraint->min)) {
+                    $messages[] = "min: {$constraint->min}";
+                }
+                if (isset($constraint->max)) {
+                    $messages[] = "max: {$constraint->max}";
+                }
+                $vparams['format'][] = '{length: ' . join(', ', $messages) . '}';
+                break;
+            case 'Choice':
+                $choices = $this->getChoices($constraint, $className);
+                $format = '[' . join('|', $choices) . ']';
+                if ($constraint->multiple) {
+                    $vparams['actualType'] = DataTypes::COLLECTION;
+                    $vparams['subType'] = DataTypes::ENUM;
+                    $messages = array();
+                    if (isset($constraint->min)) {
+                        $messages[] = "min: {$constraint->min} ";
+                    }
+                    if (isset($constraint->max)) {
+                        $messages[] = "max: {$constraint->max} ";
+                    }
+                    $vparams['format'][] = '{' . join ('', $messages) . 'choice of ' . $format . '}';
+                } else {
+                    $vparams['actualType'] = DataTypes::ENUM;
+                    $vparams['format'][] = $format;
+                }
+                break;
+            case 'Regex':
+                if ($constraint->match) {
+                    $vparams['format'][] = '{match: ' . $constraint->pattern . '}';
+                } else {
+                    $vparams['format'][] = '{not match: ' . $constraint->pattern . '}';
+                }
+                break;
+            case 'All':
+                foreach ($constraint->constraints as $childConstraint) {
+                    if ($childConstraint instanceof Type) {
+                        $nestedType = $childConstraint->type;
+                        $exp = explode("\\", $nestedType);
+                        if (!class_exists($nestedType)) {
+                            $nestedType = substr($className, 0, strrpos($className, '\\') + 1).$nestedType;
+
+                            if (!class_exists($nestedType)) {
+                                continue;
+                            }
+                        }
+
+                        $vparams['dataType']   = sprintf("array of objects (%s)", end($exp));
+                        $vparams['actualType'] = DataTypes::COLLECTION;
+                        $vparams['subType']    = $nestedType;
+                        $vparams['class']      = $nestedType;
+
+                        if (!in_array($nestedType, $visited)) {
+                            $visited[] = $nestedType;
+                            $vparams['children'] = $this->doParse($nestedType, $visited,$vparams['version'],$vparams['groups']);
+                        }
+                    }
+                }
+                break;
+        }
+
+        $vparams = $this->parseCountConstraint($constraint, $vparams);
+        $vparams = $this->parseDataType($vparams);
+        return $vparams;
     }
 
     /**

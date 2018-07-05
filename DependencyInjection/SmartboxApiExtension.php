77d7be1aa7814c6d7058df8c9ae1ef0e0fd66af8
Gary PEGEOT <gary.pegeot@smartbox.com>
2018-07-05 15:15:35 +0100

    CS fix.

--- DependencyInjection/SmartboxApiExtension.php
+++ DependencyInjection/SmartboxApiExtension.php
@@ -94,7 +94,6 @@ class SmartboxApiExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 

aec822d7fdb6905a240de55c6514370236672b83
Jason Machado Silva <jason.silva@smartbox.com>
2019-12-03 16:41:47 +0000

    Throw exception when trying to directly access property that does not exist.

--- Services/ApiConfigurator.php
+++ Services/ApiConfigurator.php
@@ -361,6 +361,9 @@ class ApiConfigurator
      */
     public function getSuccessCodeDescription($key)
     {
+        if (!isset($this->successCodes[$key])) {
+            throw new \OutOfRangeException('Requested code does not exist');
+        }
         return $this->successCodes[$key];
     }
 

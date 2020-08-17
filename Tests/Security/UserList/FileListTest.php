e9c91c3f8e9841feef1c806e7f6b89caf5d48ba6
Andres Rey <andres.rey@smartbox.com>
2020-08-17 17:28:26 +0100

    FileList: Amend tests and remove tests about caching. (Service provides no visibility to the caching service)

--- Tests/Security/UserList/FileListTest.php
+++ Tests/Security/UserList/FileListTest.php
@@ -2,17 +2,15 @@
 
 namespace Smartbox\ApiBundle\Tests\Security\UserList;
 
-use PHPUnit\Framework\TestCase;
-use Psr\Cache\CacheItemPoolInterface;
+use Smartbox\ApiBundle\DependencyInjection\SmartboxApiExtension;
 use Smartbox\ApiBundle\Security\User\ApiUser;
 use Smartbox\ApiBundle\Security\UserList\FileList;
-use Symfony\Component\Cache\Adapter\ArrayAdapter;
-use Symfony\Component\Cache\Exception\InvalidArgumentException;
+use Smartbox\ApiBundle\Tests\BaseKernelTestCase;
 
 /**
  * @group user-provider
  */
-class FileListTest extends TestCase
+class FileListTest extends BaseKernelTestCase
 {
     /**
      * @var string
@@ -20,26 +18,11 @@ class FileListTest extends TestCase
     private static $fixtureDir;
 
     /**
-     * @var ArrayAdapter
-     */
-    private $cache;
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function setUp()
+     * Basic get test. Users should be able to be retrieved with valid configuration.
+     **/
+    public function testGet()
     {
-        $this->cache = new ArrayAdapter();
-    }
-
-    /**
-     * @param string $ext
-     *
-     * @dataProvider provideSupportedExtensions
-     */
-    public function testGet($ext)
-    {
-        $list = $this->getFileList("valid_config.$ext", "passwords.$ext");
+        $list = $this->getFileList();
 
         /** @var ApiUser $user */
         $user = $list->get('regular');
@@ -54,28 +37,14 @@ class FileListTest extends TestCase
     }
 
     /**
-     * @param string $ext
-     *
-     * @dataProvider provideSupportedExtensions
+     * Basic has test. Service should tell if the user is present or not.
      */
-    public function testHas($ext)
+    public function testHas()
     {
-        $list = $this->getFileList("valid_config.$ext", "passwords.$ext");
+        $list = $this->getFileList();
 
         $this->assertTrue($list->has('admin'), 'Admin should be here.');
         $this->assertFalse($list->has('zboob'), 'Zboob should not be here.');
-    }
-
-    /**
-     * @throws \Psr\Cache\InvalidArgumentException
-     */
-    public function testBuildCache()
-    {
-        $key = \sprintf('%s.admin', FileList::CACHE_PREFIX);
-
-        $this->assertFalse($this->cache->hasItem($key), "Key \"$key\" should not exists before cache building.");
-        $this->getFileList('valid_config.json', 'passwords.json')->buildCache();
-        $this->assertTrue($this->cache->hasItem($key), "Key \"$key\" should exists after cache building.");
     }
 
     /**
@@ -107,50 +76,21 @@ class FileListTest extends TestCase
     }
 
     /**
-     * @expectedException \RuntimeException
-     * @expectedExceptionMessage Unable to fetch "admin" user: "Something happen".
-     */
-    public function testCacheFailure()
-    {
-        $this->cache = $this->getMockBuilder(CacheItemPoolInterface::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->cache->expects($this->once())
-            ->method('getItem')
-            ->willThrowException(new InvalidArgumentException('Something happen'));
-
-        $this->getFileList('valid_config.json', 'passwords.json')->get('admin');
-    }
-
-    /**
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage Unable to find "foo_404" user.
+     * Test if an exception is raised when an user cannot be found.
      */
     public function testUnknownUser()
     {
-        $this->getFileList('valid_config.json', 'passwords.json')->get('foo_404');
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Unable to find "foo_404" user.');
+        $this->getFileList()->get('foo_404');
     }
 
     /**
-     * @return array
-     */
-    public function provideSupportedExtensions()
-    {
-        return [
-            'JSON' => ['json'],
-            'YAML' => ['yml'],
-        ];
-    }
-
-    /**
-     * @param string $users
-     * @param string $password
-     *
      * @return FileList
      */
-    private function getFileList($users, $password)
+    private function getFileList()
     {
-        return new FileList("{$this->getFixtureDir()}/$users", "{$this->getFixtureDir()}/$password", $this->cache);
+        return $this->getContainer()->get(SmartboxApiExtension::SERVICE_ID_FILE_LIST);
     }
 
     /**
